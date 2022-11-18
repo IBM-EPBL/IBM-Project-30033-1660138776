40c59b08c125c77214080a2d6f449726b98b9b94
AarthiGgithub <113244159+AarthiGgithub@users.noreply.github.com>
2022-11-19 00:20:54 +0530

    Update app.py

--- Project Development Phase/Sprint1/Code/app.py
+++ Project Development Phase/Sprint1/Code/app.py
@@ -1,1 +1,68 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import ibm_db
+import re
+from random import * 
+from flask_mail import Mail, Message
+app = Flask(__name__)
+mail = Mail(app) # instantiate the mail class
+
+# configuration of mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'nilavarasi2002@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ipergvvhzblqfnpd'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+otp = randint(000000,999999)
+
+def test_predict_image_url():
+    stub = service_pb2_grpc.V2Stub(ClarifaiChannel.get_grpc_channel())
+
+    req = service_pb2.PostModelOutputsRequest(
+        model_id=GENERAL_MODEL_ID,
+        inputs=[
+            resources_pb2.Input(
+                data=resources_pb2.Data(image=resources_pb2.Image(url=DOG_IMAGE_URL))
+            )
+        ],
+    )
+
+    response = post_model_outputs_and_maybe_allow_retries(stub, req, metadata=metadata())
+    print(response)
+    raise_on_failure(response)
+
+    assert len(response.outputs[0].data.concepts) > 0
+app.secret_key = 'a'
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=ba99a9e6-d59e-4883-8fc0-d6a8c9f7a08f.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=31321;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=pvw86690;PWD=zJNTZLGkY3yuoy3X",'','')
+@app.route("/")
+@app.route('/home')
+def home():
+   return render_template('index.html')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    global userid
+    msg = ''
+    if request.method == 'POST'and 'email' in request.form and 'password1' in request.form:
+        email = request.form['email']
+        password1 = request.form['password1']
+        stmt = ibm_db.prepare(conn,'SELECT * FROM regi WHERE email = ? AND password1 = ?')
+        ibm_db.bind_param(stmt,1,email)
+        ibm_db.bind_param(stmt,2,password1)        
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_tuple(stmt)
+        if account:
+            session['id'] = account[0]
+            userid =  account[0]
+            session['email'] = account[1]
+            msg = 'Logged in successfully !'
+            return redirect(url_for('button'))
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+@app.route('/button')
+def button():
+    return render_template('button.html')
+  if __name__ == '__main__':
+    app.run(debug = True)
 
